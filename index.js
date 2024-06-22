1cc7e78c06b2fe3761513c87733a96617ee11026
Jayesh Ranjan <jayeshsinha20112001@gmail.com>
2024-06-22 22:40:45 +0530

    fourth-commit

--- index.js
+++ index.js
@@ -1,15 +1,44 @@
 const express = require('express')
 const app = express()
+const model = require('./models/product.model.js');
 const mongoose = require('mongoose');
+const Product = require('./models/product.model.js');
+
+app.use(express.json());
 
 app.get('/', (req, res) => {
     res.send('Hello from node API Server updated');
 });
 
+app.get('/api/products', async (req, res) => {
+    try {
+        const products = await Product.find({});
+        res.status(200).json(products);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+        
+    }
+});
 
-app.post('/api/Products',(req, res) =>{
-    console.log(req.body);
-    res.send(res.body);
+app.get('/api/products/:id', async (req, res) => {
+    try {
+        const { id } = req.params;
+        const product = await Product.findById(id);
+        res.status(200).json(product);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+
+app.post('/api/Products', async (req, res) =>{
+    try {
+        const product = await Product.create(req.body);
+        res.status(200).json(product);
+    } catch(error) {
+        res.status(500).json({message: error.message});
+    }
+
 });
 
 mongoose.connect("mongodb+srv://admin:PuvVUOJJ0xUBYK1f@backenddb.qquq32e.mongodb.net/Node-API?retryWrites=true&w=majority&appName=BackendDB")
@@ -21,4 +50,35 @@ mongoose.connect("mongodb+srv://admin:PuvVUOJJ0xUBYK1f@backenddb.qquq32e.mongodb
 })
 .catch(() => {
     console.log("Connection failed!");
+});
+
+// update the product
+app.put('/api/product/:id', async(req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndUpdate(id, req.body);
+        if(!product){
+            return res.status(404).json({message: "Product not found"});
+        }
+        const updatedProduct = await Product.findById(id);
+        res.status(200).json(updatedProduct);
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
+});
+
+// delete a product
+
+app.delete('/api/product/:id', async (req, res) => {
+    try {
+        const {id} = req.params;
+        const product = await Product.findByIdAndDelete(id);
+        if(!product){
+            return res.status(404).json({message: "Product not found"});
+        }
+        res.status(200).json({message: "Product deleted successfully"});
+        
+    } catch (error) {
+        res.status(500).json({message: error.message});
+    }
 })
